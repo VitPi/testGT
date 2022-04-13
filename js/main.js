c5e0c616daf1c23f31d0916d63cb8d4d7f79366b
ViTali <cpaaaik@gmail.com>
2022-04-13 11:10:34 +0300

    add: burger

--- js/main.js
+++ js/main.js
@@ -1,44 +1,55 @@
+//
+
+"use strict";
+
+
+//
+
+
+//
+
+
+// const swiper = new Swiper('.swiper', {
+//     // Optional parameters
+//     direction: 'horizontal',
+//     loop: true,
+//
+//     // If we need pagination
+//     // pagination: {
+//     //     el: '.swiper-pagination',
+//     // },
+//
+//     // Navigation arrows
+//     navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev',
+//     },
+//
+//     // And if we need scrollbar
+//     // scrollbar: {
+//     //     el: '.swiper-scrollbar',
+//     // },
+// });
+
+
+const clickOnLink = document.querySelectorAll('.services');
+
+
+for (const clickOnLinkElement of clickOnLink) {
+
+    clickOnLinkElement.addEventListener('click', () => {
+
+        clickOnLinkElement.classList.toggle('click')
+
+        clickOnLinkElement.nextElementSibling.classList.toggle('services-item');
+
+    })
+
+
+}
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-
-    // If we need pagination
-    // pagination: {
-    //     el: '.swiper-pagination',
-    // },
-
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-
-    // And if we need scrollbar
-    // scrollbar: {
-    //     el: '.swiper-scrollbar',
-    // },
-});
 
 
 
