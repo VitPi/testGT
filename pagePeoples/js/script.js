a8d015ed7da5f770a45f3abbc94ca42b63eebb5b
ViTali <cpaaaik@gmail.com>
2022-04-18 14:07:41 +0300

    add: button on carusel

--- pagePeoples/js/script.js
+++ pagePeoples/js/script.js
@@ -1,31 +1,28 @@
-
-
-
 //
 
 
+"use strict";
 
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
 
-    // If we need pagination
-    pagination: {
-        el: '.swiper-pagination',
-    },
 
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
 
-    // And if we need scrollbar
-    scrollbar: {
-        el: '.swiper-scrollbar',
-    },
-});
+    // setInterval(() => {
+    //
+    //     const carousel = document.querySelector('.carousel'),
+    //         screenWidth = window.innerWidth;
+    //
+    //     if (screenWidth > 800) {
+    //         carousel.setAttribute('data-bs-ride', 'none');
+    //     }
+    //
+    //
+    // }, 1000)
+
+
+
+
+
+
 
 
 
