4dc91c476fd4403352d32085c58fedac24542cb1
ViTali <cpaaaik@gmail.com>
2022-04-20 16:53:06 +0300

    add: services

--- pagePeoples/peoples/leusLydia/js/script.js
+++ pagePeoples/peoples/leusLydia/js/script.js
@@ -22,18 +22,12 @@ let opt = $(".polzunok-1").data().uiSlider.options,
     raz = opt.max - min,
     zero = 0;
 
+const arrTop = ['1997 г.', '2000 г.', '2007 г.', '2018 г.', '2021 г.',], // верхний ряд
+    arrBottom = ['1985 г.', '2005 г.', '2009 г.', '2019 г.']; // нижний ряд
 
 for (let i = 1; i < raz; i++) {
-
-
     $(".polzunok-1").append($('<b class="click-on-b">' + '</b>').css('left', (i / raz * 100) + '%'));
-
-
-    const arrBottom = ['1985 г.', '2005 г.', '2009 г.', '2019 г.'], // нижний ряд
-        arrTop = ['1997 г.', '2000 г.', '2007 г.', '2018 г.', '2021 г.',] // верхний ряд
-
     if (i % 2 === 0) {
-
         $(".polzunok-1").append($('<span id="b">' + arrBottom[zero] + '</span>').css('left', (i / raz * 100) + '%'));
         zero++
     } else {
@@ -42,15 +36,55 @@ for (let i = 1; i < raz; i++) {
 
 }
 
-
 const elementB = document.querySelectorAll('.click-on-b'),
     output = document.querySelector('.year-skill'),
-    la = document.querySelector('.la');
+    la = document.querySelector('.prof'),
+    left = document.querySelector('.left'),
+    right = document.querySelector('.right');
+
+function joinArr(arr, arr2) {
+
+    const masArr = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        masArr.push(arr[i])
+        masArr.push(arr2[i])
+    }
+
+    let numberPos = masArr.indexOf(output.innerHTML);   // номер элимента в массиве
+
+
+    left.addEventListener('click', () => {
+
+        if (numberPos >= 0) {
+            output.innerHTML = masArr[numberPos]
+            numberPos--
+            if (numberPos < 0) {
+                numberPos = 0
+            }
+        }
+    })
+
+
+    right.addEventListener('click', () => {
+
+        if (numberPos <= masArr.length - 2) {
+            output.innerHTML = masArr[numberPos]
+            numberPos++
+            if (numberPos > masArr.length - 2) {
+                numberPos = masArr.length - 2
+            }
+        }
+    })
+
+
+}
+
+joinArr(arrTop, arrBottom)
 
 
 for (const elementBKey in elementB) {
     let el = elementB[elementBKey];
-
 
     el.addEventListener('click', () => {
 
@@ -60,6 +94,25 @@ for (const elementBKey in elementB) {
     })
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
