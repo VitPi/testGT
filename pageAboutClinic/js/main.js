b3959216081e0c930bbfec6330e25f1dc80420fb
ViTali <cpaaaik@gmail.com>
2022-04-22 11:27:38 +0300

    add: edit the slide

--- pageAboutClinic/js/main.js
+++ pageAboutClinic/js/main.js
@@ -9,29 +9,6 @@
 //
 
 
-// const swiper = new Swiper('.swiper', {
-//     // Optional parameters
-//     direction: 'horizontal',
-//     loop: true,
-//
-//     // If we need pagination
-//     // pagination: {
-//     //     el: '.swiper-pagination',
-//     // },
-//
-//     // Navigation arrows
-//     navigation: {
-//         nextEl: '.swiper-button-next',
-//         prevEl: '.swiper-button-prev',
-//     },
-//
-//     // And if we need scrollbar
-//     // scrollbar: {
-//     //     el: '.swiper-scrollbar',
-//     // },
-// });
-
-
 new Swiper('.swiper');
 
 
